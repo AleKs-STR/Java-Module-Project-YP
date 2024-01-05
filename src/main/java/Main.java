a2030df27e09eff181c0dbc907d3ad033de24356
AleKS <aleksandrstrocenkov@gmail.com>
2024-01-05 20:26:56 +0500

    Калькулятор счета

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,62 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        int numberPeople;
+
+        while (true) {
+            System.out.print("На скольких человек разделить счет?: ");
+
+            if (scanner.hasNextInt()) {
+                numberPeople = scanner.nextInt();
+                if (numberPeople <= 1) {
+                    System.out.println("Некорректное значение! Введите количество людей больше 1 ");
+                } else {
+                    break;
+                }
+            } else {
+                System.out.println("Некоректное значение! Введите целое число.");
+            }
+        }
+        System.out.println("Создан калькулятор на " + numberPeople + " человек");
+
+        List<Product> products = new ArrayList<>();
+
+        while (true) {
+            System.out.println("Введите название товара или 'Завершить' для окончания: ");
+            String name = scanner.next();
+            if (name.equalsIgnoreCase("Завершить")) {
+                break;
+            }
+            System.out.println("Введите стоимость товара в формате рубли.копейки: ");
+            double price = scanner.nextDouble();
+
+            Product product = new Product(name, price);
+            products.add(product);
+            System.out.println("Товар успешно добавлен.");
+            System.out.println("Хотите добавить еще один товар? (Введите 'Завершить' для окончания): ");
+            String continueInput = scanner.next();
+
+            if (continueInput.equalsIgnoreCase("Завершить")) {
+                break;
+            }
+        }
+        System.out.println("Список товаров: ");
+        double total = 0;
+
+        for (Product product : products) {
+            System.out.println(product.getName() + " - " + product.getPrice());
+            total += product.getPrice();
+        }
+
+        System.out.println("Общая сумма: " + total);
+
+        double amountPerPerson = total / numberPeople;
+        String currency = (int) amountPerPerson == 1 ? "рубль" : "рубля";
+        System.out.println("Каждому человеку необходимо заплатить: " + amountPerPerson + " " + currency);
     }
 }
+
---
+++ src/main/java/Product.java
@@ -0,0 +1,15 @@
+class Product {
+    public String name;
+    public double price;
+
+    public Product (String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public double getPrice() {
+        return price;
+    }
+}
